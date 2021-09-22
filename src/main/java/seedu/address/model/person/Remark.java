5a4981a6e588c5a9635cc3cfe33929c477989387
LuoZhijie-tom <e0492460@u.nus.edu>
2021-09-22 19:46:26 +0800

    Add Java doc in Remark

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a remark object.
+     * @param remark The remark that is going to be added.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
